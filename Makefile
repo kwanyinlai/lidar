8f24778259b2538c059b0a617e9a1b66a21fddad
kwanyinlai <laikwanyin06@gmail.com>
2026-03-07 00:33:46 -0500

    start of a3

---
+++ Makefile
@@ -0,0 +1,31 @@
+CC = gcc
+CFLAGS = -Wall -Wextra -O2 -std=c99
+BUILD_DIR = build
+
+UNAME := $(shell uname)
+ifeq ($(UNAME), Darwin)
+    CFLAGS += -DGL_SILENCE_DEPRECATION
+    LIBS = -framework OpenCL -framework GLUT -framework OpenGL -lm
+else
+    LIBS = -lGL -lGLU -lglut -lm
+endif
+
+SRCS = main.c
+OBJS = $(BUILD_DIR)/$(SRCS:.c=.o)
+TARGET = $(BUILD_DIR)/lidar
+HEADERS = scene.h vec3.h bvh.h
+
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+	$(CC) $(CFLAGS) -o $@ $^ $(LIBS)
+
+$(BUILD_DIR)/%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(BUILD_DIR)/main.o: main.c $(HEADERS)
+	$(CC) $(CFLAGS) -c main.c -o $(BUILD_DIR)/main.o
+
+clean:
+	rm -f $(BUILD_DIR)/*.o $(BUILD_DIR)/*.d $(BUILD_DIR)/$(TARGET)
---
+++ vec3.h
@@ -0,0 +1,63 @@
+# ifndef VEC3_H
+# define VEC3_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/* 
+This file contains the implementation of basic vector operations for 3D vectors.
+*/
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Vector3;
+
+static inline Vector3 vector3_add(Vector3 a, Vector3 b) {
+    Vector3 result;
+    result.x = a.x + b.x;
+    result.y = a.y + b.y;
+    result.z = a.z + b.z;
+    return result;
+}
+
+static inline Vector3 vector3_subtract(Vector3 a, Vector3 b) {
+    Vector3 result;
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+    result.z = a.z - b.z;
+    return result;
+}
+
+static inline Vector3 vector3_scale(Vector3 v, float scalar) {
+    Vector3 result;
+    result.x = v.x * scalar;
+    result.y = v.y * scalar;
+    result.z = v.z * scalar;
+    return result;
+}
+
+static inline float vector3_dot(Vector3 a, Vector3 b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+static inline Vector3 vector3_cross(Vector3 a, Vector3 b) {
+    Vector3 result;
+    result.x = a.y * b.z - a.z * b.y;
+    result.y = a.z * b.x - a.x * b.z;
+    result.z = a.x * b.y - a.y * b.x;
+    return result;
+}
+
+static inline float vector3_magnitude(Vector3 v) {
+    return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+static inline Vector3 vector3_normalize(Vector3 v) {
+    float mag = vector3_magnitude(v);
+    if (mag == 0) return (Vector3){0, 0, 0};
+    return vector3_scale(v, 1.0f / mag);
+}
+
+# endif // VEC3_H
